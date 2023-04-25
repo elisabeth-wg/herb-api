dd724f7522a477f5a7ba6e221d46130cb6306d70
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-04-25 14:45:14 -0500

    more work towards railway

--- herb-api/server.js
+++ herb-api/server.js
@@ -1,54 +1,45 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const PORT = 8000
 
-// const PORT = 8000
-//to try to get on cyclic
-const PORT = process.env.port || 5000;
-
+app.use(cors())
 
 const rappers = {
     '21 savage': {
         'age': 29,
         'birthName': 'Shéyaa Bin Abraham-Joseph',
-        'birthLocation': 'London, England'  
+        'birthLocation': 'London, England'
     },
-    'chance the rapper': {
-        'age': 30,
-        'birthName': 'Chancelor Johnathan Bennett',
-        'birthLocation': 'Chicago, Illinois'
+    'chance the rapper':{
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois' 
     },
-    'unknown': {
+    'unknown':{
         'age': 0,
         'birthName': 'unknown',
         'birthLocation': 'unknown'
     }
 }
-
-
-app.get('/', (request, response) => {
+app.get('/', (request, response)=>{
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:name', (request, response) => {
+app.get('/api/:name',(request,response)=>{
     const rapperName = request.params.name.toLowerCase()
-    if (rappers[rapperName] ) {
+
+    if( rappers[rapperName] ){
         response.json(rappers[rapperName])
-    } else {
+    }else{
         response.json(rappers['unknown'])
     }
+    
 })
 
-
-to get up on cyclic
-app.listen(PORT, async () => {
-  try {
-    await connect();
-    console.log(`Listening at ${PORT}`);
-  } catch (e) {
-    console.log(e.message);
-  }
-});
-
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
+})
 
 // app.listen(process.env.PORT || PORT, () => {
 //     console.log(`The server is now running on port ${PORT}! Betta Go Catch It!`)
